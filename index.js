48e68130d23527c433f745938a11feda1ac51b1d
Maurice <mauriceglenn2@gmail.com>
2023-10-26 15:39:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//API Key: 4bd2830168965fce4c7000934dae247a
+
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//Coordinates by zip/post
+//api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+
+//api.openweathermap.org/geo/1.0/zip?zip=78227,1&appid=4bd2830168965fce4c7000934dae247a
+
+console.log("hello world");
