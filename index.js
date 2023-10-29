6a491fa043d826ef4202c83ab343885ef88dadb4
Maurice <mauriceglenn2@gmail.com>
2023-10-28 19:04:02 -0500

    Starting API in JS file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 //AccuWeather API Key: jzepoFpxGnzzlfOpukbXNpWnkrmplkKD
 //get location: "http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=jzepoFpxGnzzlfOpukbXNpWnkrmplkKD&q=78227";
 console.log("hello world");
+
+
