c2839b82cdce0ad78e2638a29a5c91df5d6b93c4
Maurice <mauriceglenn2@gmail.com>
2023-10-30 13:04:59 -0500

    was able to fetch data

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 //AccuWeather API Key: jzepoFpxGnzzlfOpukbXNpWnkrmplkKD
 //get location: "http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=jzepoFpxGnzzlfOpukbXNpWnkrmplkKD&q=78227";
 //5 day forcast: "http://dataservice.accuweather.com/forecasts/v1/daily/5day/34150_PC?apikey=jzepoFpxGnzzlfOpukbXNpWnkrmplkKD"
-console.log("hello world");
 
-// async function zipCode (){
-//   const response = await fetch(
-//     "http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=jzepoFpxGnzzlfOpukbXNpWnkrmplkKD&q="
-//   );
-//   const data = await response.json();
-//   console.log(data);
-// }
-
-// zipCode()
-
-// async function main (){
-//     const response = await fetch(
-//       "http://dataservice.accuweather.com/forecasts/v1/daily/5day/34150_PC?apikey=jzepoFpxGnzzlfOpukbXNpWnkrmplkKD"
-//     );
-//     const data = await response.json()
-//     console.log(data)
-// }
-
-// main()
-
+// Get a reference to the input field and the fetch button
+const zipCodeInput = document.getElementById("zipCodeInput");
+const searchButton = document.getElementById("searchButton");
+const high = document.querySelector(".high")
+const low = document.querySelector(".low")
+// const location = document.querySelector(".weekly--para--location");
+let temps;
 
 async function getLocationData(zipCode) {
   try {
@@ -53,17 +39,49 @@ async function getWeatherForecast(locationKey) {
 }
 
 async function main() {
-  const zipCode = "34150_PC"; // Replace with the actual ZIP code you want to use.
-
-  // Get location data
-  const locationData = await getLocationData(zipCode);
-  if (locationData) {
-    // Extract the location key from the location data
-    const locationKey = locationData[0]?.Key;
-
-    // Get weather forecast using the location key
-    await getWeatherForecast(locationKey);
+  const zipCode = zipCodeInput.value; // Get ZIP code from user input
+  if (zipCode) {
+    const locationData = await getLocationData(zipCode);
+    if (locationData) {
+      const locationKey = locationData[0]?.Key;
+      await getWeatherForecast(locationKey);
+    }
+  } else {
+    console.log("Please enter a ZIP code.");
   }
 }
 
 main();
+
+// Add an event listener to the fetch button
+searchButton.addEventListener("click", async () => {
+  const zipCode = zipCodeInput.value;
+  console.log("Button Clicked");
+  if (zipCode) {
+    try {
+      const locationData = await getLocationData(zipCode);
+      if (locationData) {
+        const locationKey = locationData[0]?.Key;
+        await getWeatherForecast(locationKey);
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching data:", error);
+    }
+  } else {
+    console.log("Please enter a ZIP code.");
+  }
+});
+
+
+//placing data in htlm
+// const tempsHtml = temps.map(temp =>{
+//     return `<div class="temp__card">
+//             <img src="" alt="">
+//             <h1 class="weekly--para">day 1 Temp</h1>
+//             <h1 class="weekly--para--location">Location</h1>
+//             <div class="card__split"></div>
+//             <p class="high">High: </p>
+//             <p class="low">Low: </p>
+//         </div>`
+// })
+
